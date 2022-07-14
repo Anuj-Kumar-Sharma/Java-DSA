eba45e336316cbf523f515e7ede1ebdb9be646c6
Chandan Maharana <85704460+lifeforcoding@users.noreply.github.com>
2022-07-15 01:37:41 +0530

    Update the condition of left part sorted

--- binarySearchBasics/MainClass.java
+++ binarySearchBasics/MainClass.java
@@ -44,7 +44,7 @@ public class MainClass {
 		while(l <= r) {
 			int mid = l + (r-l)/2;
 			if(a[mid] == key) return mid;
-			if(a[l] < a[mid]) { // left part sorted
+			if(a[l] <= a[mid]) { // left part sorted
 				if(key >= a[l] && key < a[mid]) { // left side present
 					r = mid-1;
 				} else { // right side present
